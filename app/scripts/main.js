b4f9cb6c8ba537496f63c8ff35e35bcba288f4f5
Bersoriano <bersoriano@me.com>
2014-12-28 23:08:49 -0600

    new fancy overlay

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,13 @@
 console.log('\'Allo \'Allo!');
+
+
+
+$(document).ready(function(){
+
+setTimeout(function() {
+    // Do something after 5 seconds
+	$('.overlay').addClass('fadeOut').delay(2000);
+	$('.overlay').fadeOut(1000).delay(1000);
+}, 1000);
+
+});
